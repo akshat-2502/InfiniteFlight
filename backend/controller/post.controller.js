a025170bf8b677b972e7565ba0d7f73c440e1afc
akshat-2502 <khulbeakshat@gmail.com>
2025-07-02 17:38:47 +0530

    get all post api is working

--- backend/controller/post.controller.js
+++ backend/controller/post.controller.js
@@ -1,4 +1,4 @@
-import Post from "../models/Post";
+import Post from "../models/Post.js";
 
 export const createPost = async (req, res) => {
   try {
@@ -6,7 +6,7 @@ export const createPost = async (req, res) => {
     if (!caption) {
       return res.status(400).json({ message: "Caption is required" });
     }
-    const post = new Post.create({
+    const post = new Post({
       caption,
       image,
       postedBy: req.user._id,
@@ -14,9 +14,53 @@ export const createPost = async (req, res) => {
 
     await post.save();
 
-    res.statis(201).json({ message: "Post Created Successfully", post });
+    res.status(201).json({ message: "Post Created Successfully", post });
   } catch (err) {
     console.error("Error creating post:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+//COMMENT
+
+export const addComment = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const { postId } = req.params;
+
+    if (!text) {
+      return res.status(400).json({ message: "Comment text is required" });
+    }
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post Not Found" });
+    }
+    post.comments.push({
+      text,
+      commentedBy: req.user._id,
+    });
+
+    await post.save();
+    res.status(201).json({ message: "Comment added", comments: post.comments });
+  } catch (err) {
+    console.error("Error adding comment:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+//Getting All Post
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .populate("postedBy", "username country") //show user and country
+      .populate("comments.commentedBy", "username country") //coment
+      .sort({ createdAt: -1 }); //latest first
+
+    res.status(200).json(posts);
+  } catch (err) {
+    console.error("Error fetching posts:", err);
     res.status(500).json({ message: "Server error" });
   }
 };
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { authenticateUser } from "../middleware/authMiddleware";
-import { createPost } from "../controller/post.controller";
+import { authenticateUser } from "../middleware/authMiddleware.js";
+import { addComment, createPost } from "../controller/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", authenticateUser, createPost);
+router.post("/:postId/comment", authenticateUser, addComment);
 
 export default router;
