d0f7b0e628b4a8290e0c84214f10597a24705b16
akshat-2502 <khulbeakshat@gmail.com>
2025-07-02 17:45:18 +0530

    get all post routing done testing is left

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { authenticateUser } from "../middleware/authMiddleware.js";
-import { addComment, createPost } from "../controller/post.controller.js";
+import {
+  addComment,
+  createPost,
+  getAllPosts,
+} from "../controller/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", authenticateUser, createPost);
 router.post("/:postId/comment", authenticateUser, addComment);
+router.get("/", getAllPosts);
 
 export default router;
