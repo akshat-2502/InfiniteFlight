6968dffbfebae604c94bebdbf3bdb586cbbb56f1
akshat-2502 <khulbeakshat@gmail.com>
2025-07-03 22:06:49 +0530

    frontend started

---
+++ frontend/src/pages/Authorization.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Register from "../components/Register";
+
+const Authorization = () => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default Authorization;
