e783adde8a80342eae7cb49985b1415bb60f0c91
akshat-2502 <khulbeakshat@gmail.com>
2025-07-06 22:23:53 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 connectDatabase();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
